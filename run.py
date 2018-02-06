c5606e8ed37e9f4dcf4d35771f036dacfeebd321
ektormak <ektor.mak@gmail.com>
2018-02-06 23:55:38 +0000

    add placeholder

---
+++ run.py
@@ -0,0 +1,13 @@
+"""
+ simple quadratic function
+ ||X * \theta - y||_{2}^{2}
+
+10x10 matrices X and 10 dimenstional vectors y
+ the params thera are also 10 dim
+
+coordinate-wise LSTM:
+
+ each LSTM get one gradient coordinate as input return the one coordinate as output
+ all of the LSTMs have the same parameters \phi but different hidden states
+"""
+
