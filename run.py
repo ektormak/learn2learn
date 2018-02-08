e15e997cbe08253e09bef5061e7e3d896fbe72ba
Ektor Makridis <ektor.mak@gmail.com>
2018-02-08 23:17:04 +0000

    Update run.py

--- run.py
+++ run.py
@@ -1,10 +1,4 @@
 """
- simple quadratic function
- ||X * \theta - y||_{2}^{2}
-
-10x10 matrices X and 10 dimenstional vectors y
- the params thera are also 10 dim
-
 coordinate-wise LSTM:
 
  each LSTM get one gradient coordinate as input return the one coordinate as output
